d126984e1c3590c9570d11194487558188986d48
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-08 02:17:29 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   has_attached_file :album_cover
+  validates_attachment_content_type :album_cover, content_type: /\Aimage\/.*\z/
 
   def artist_name
     self.try(:artist).try(:name)
